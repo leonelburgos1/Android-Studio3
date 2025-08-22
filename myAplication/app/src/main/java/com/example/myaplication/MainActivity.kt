18427573ae2d568588e8a20cca94e968b38050a4
alexander <alezander.burgos@gmail.com>
2025-08-22 18:04:37 -0500

    avance 3

--- myAplication/app/src/main/java/com/example/myaplication/MainActivity.kt
+++ myAplication/app/src/main/java/com/example/myaplication/MainActivity.kt
@@ -1,33 +1,42 @@
 package com.example.myaplication
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val boton1 = findViewById<Button>(R.id.button)
-        // otra forma val boton1 = findViewById<Button>(R.id.button)
-        val editText = findViewById<EditText>(R.id.editTextText)
-        val textview = findViewById<TextView>(R.id.textView3)
 
-        boton1.setOnClickListener {
-            val texto = editText.text.toString()
-            textview.text = texto
-        }
+        val pesoEditText = findViewById<EditText>(R.id.peso)       // ID del campo de peso
+        val estaturaEditText = findViewById<EditText>(R.id.estatura)     // ID del campo de estatura
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView3)
 
+        button.setOnClickListener {
+            val pesoTexto = pesoEditText.text.toString()
+            val estaturaTexto = estaturaEditText.text.toString()
+
+            val peso = pesoTexto.toFloatOrNull()
+            val estatura = estaturaTexto.toFloatOrNull()
+
+            if (peso != null && estatura != null && estatura > 0) {
+                val imc = peso / (estatura * estatura)
+                val mensaje = when {
+                    imc < 18.5 -> "Bajo peso"
+                    imc < 25 -> "Peso normal"
+                    imc < 30 -> "Sobrepeso"
+                    else -> "Obesidad"
+                }
+
+                textView.text = "Tu IMC es %.2f: %s".format(imc, mensaje)
+            } else {
+                Toast.makeText(this, "Por favor ingresa peso y estatura válidos", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
